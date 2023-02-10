5e463bff31bcf7439b1115b522ee90ce54b36845
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-10 18:11:22 +0700

    Server initialize

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 #![allow(dead_code, unused_variables)]
 
+use std::net::SocketAddr;
+
+use axum::{response::Response, Router, Server};
+use dotenv::{dotenv, var};
 use error::Error;
+use services::user::UserService;
+
+use crate::{
+    dao::Dao, db::mongo::Mongo, model::user::User, repo::user::UserRepo, routes::user::user_routes,
+};
 
 mod dao;
 mod db;
@@ -15,8 +24,40 @@ mod validation;
 pub mod web;
 
 type Result<T> = std::result::Result<T, Error>;
+type WebResult = Result<Response>;
+
+#[derive(Debug, Clone)]
+pub struct SharedState {
+    user_service: UserService,
+}
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    // Anything fails in main should crash, until all is resolved
+    dotenv().ok();
+
+    let db = Mongo::init().await?;
+    let user_dao = Dao::<User>::init(&db, "User");
+    let user_repo = UserRepo::init(&user_dao);
+    let user_service = UserService::init(&user_repo);
+
+    let port = var("PORT")
+        .expect("Cannot read the PORT in the env")
+        .parse()
+        .expect("Cannot convert PORT variable into u16");
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    let router = Router::new()
+        .merge(user_routes())
+        .with_state(SharedState { user_service });
+
+    Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
+
     println!("Hello, world!");
+
+    Ok(())
 }
