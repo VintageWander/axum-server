934982c6b3cbf48aedbdb74d397c009cf9009446
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-10 18:11:09 +0700

    First user handler

--- src/handler/user/get.rs
+++ src/handler/user/get.rs
@@ -1,1 +1,11 @@
+use axum::extract::State;
 
+use crate::{web::Web, SharedState, WebResult};
+
+pub async fn get_users_handler(
+    State(SharedState { user_service }): State<SharedState>,
+) -> WebResult {
+    let users = user_service.get_users().await?;
+
+    Ok(Web::ok("Get all users success", users))
+}
