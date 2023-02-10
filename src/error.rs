a9ba873d5147f5ce94106275ff36b79ce987ae09
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-10 18:11:00 +0700

    Added some errors

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,7 @@
+use axum::response::{IntoResponse, Response};
 use thiserror::Error;
+
+use crate::{helper::print_validation::extract_validation_error, web::Web};
 
 #[derive(Debug, Error)]
 pub enum Error {
@@ -21,4 +24,27 @@ pub enum Error {
 
     #[error("Conflict user")]
     ConflictUser,
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        match self {
+            // Internal error
+            Error::Mongo(e) => Web::internal_error("MongoDB error", e),
+
+            // Validation error
+            Error::Fields(e) => Web::bad_request(
+                "Some of the fields are incorrect",
+                extract_validation_error(&e),
+            ),
+            Error::Field(e) => Web::bad_request("One of the field are incorrect", e),
+
+            // Custom error
+            Error::ResourceNotFound => Web::not_found(
+                "Resource not found",
+                "Users, files, folders, or any resource with the provided information could not be found",
+            ),
+            Error::ConflictUser => Web::conflict("Resource conflict", "This could mean that the resource provided already exists in the database"),
+        }
+    }
 }
