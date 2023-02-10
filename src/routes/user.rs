1206560f1d958f2507b0134325a662342062b63a
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-10 18:11:31 +0700

    Added user route code

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,1 +1,11 @@
+use axum::{routing::get, Router};
 
+use crate::{handler::user::get::get_users_handler, SharedState};
+
+pub fn user_routes() -> Router<SharedState> {
+    Router::new().nest("/user", get_users_route())
+}
+
+pub fn get_users_route() -> Router<SharedState> {
+    Router::new().route("/", get(get_users_handler))
+}
