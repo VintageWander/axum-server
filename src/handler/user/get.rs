898c50b9bf1675a7fef4f593e039c5cc10baaee4
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-14 17:54:18 +0700

    Map the list of users into user response
    To prevent leaking password

--- src/handler/user/get.rs
+++ src/handler/user/get.rs
@@ -1,11 +1,16 @@
 use axum::extract::State;
 
-use crate::{web::Web, SharedState, WebResult};
+use crate::{extractors::query_doc::UserQueryDocument, web::Web, SharedState, WebResult};
 
 pub async fn get_users_handler(
     State(SharedState { user_service }): State<SharedState>,
+    UserQueryDocument(user_query): UserQueryDocument,
 ) -> WebResult {
-    let users = user_service.get_users().await?;
+    let users = user_service
+        .get_users_by(user_query)
+        .await?
+        .map(|u| u.into_response())
+        .collect::<Vec<_>>();
 
     Ok(Web::ok("Get all users success", users))
 }
