2fab285afaa87b9efca397e52fffce5584ac9ab9
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-14 17:51:14 +0700

    Added extra errors

--- src/error.rs
+++ src/error.rs
@@ -9,6 +9,12 @@ pub enum Error {
     #[error("DB error")]
     Mongo(#[from] mongodb::error::Error),
 
+    #[error("JWT error")]
+    Jwt(#[from] jsonwebtoken::errors::Error),
+
+    #[error("Infallible error")]
+    Infallible(#[from] std::convert::Infallible),
+
     // Validation error
     #[error("Input fields error")]
     Fields(#[from] validator::ValidationErrors),
@@ -16,7 +22,18 @@ pub enum Error {
     #[error("Input field error")]
     Field(#[from] validator::ValidationError),
 
-    // Library error
+    // Bad requests error
+    #[error("Invalid json form")]
+    InvalidJson(#[from] axum::extract::rejection::JsonRejection),
+
+    #[error("Invalid query string")]
+    InvalidQuery(#[from] axum::extract::rejection::QueryRejection),
+
+    #[error("Invalid path string form")]
+    InvalidPath(#[from] axum::extract::rejection::PathRejection),
+
+    #[error("Invalid id entered")]
+    InvalidId(#[from] mongodb::bson::oid::Error),
 
     // Custom error
     #[error("Resource not found")]
@@ -24,6 +41,15 @@ pub enum Error {
 
     #[error("Conflict user")]
     ConflictUser,
+
+    #[error("Passwords mismatch")]
+    PasswordsMismatch,
+
+    #[error("Invalid Password")]
+    InvalidPassword,
+
+    #[error("Unauthorized")]
+    Unauthorized,
 }
 
 impl IntoResponse for Error {
@@ -31,13 +57,21 @@ impl IntoResponse for Error {
         match self {
             // Internal error
             Error::Mongo(e) => Web::internal_error("MongoDB error", e),
-
+            Error::Jwt(e) => Web::internal_error("JWT error occur", e),
+            Error::Infallible(e) => Web::internal_error("Infallible error", "If you see this error please let me know"),
+            
             // Validation error
             Error::Fields(e) => Web::bad_request(
                 "Some of the fields are incorrect",
                 extract_validation_error(&e),
             ),
             Error::Field(e) => Web::bad_request("One of the field are incorrect", e),
+            
+            // Bad requests error
+            Error::InvalidJson(e) => Web::bad_request("Invalid json form", e),
+            Error::InvalidQuery(e) => Web::bad_request("Invalid query form", e),
+            Error::InvalidId(e) => Web::bad_request("The id provided is invalid", e),
+            Error::InvalidPath(e) => Web::bad_request("Invalid path string provided", e),
 
             // Custom error
             Error::ResourceNotFound => Web::not_found(
@@ -45,6 +79,10 @@ impl IntoResponse for Error {
                 "Users, files, folders, or any resource with the provided information could not be found",
             ),
             Error::ConflictUser => Web::conflict("Resource conflict", "This could mean that the resource provided already exists in the database"),
+            Error::PasswordsMismatch => Web::bad_request("Passwords mismatch", "Password and confirm password fields are incorrect"),
+            Error::InvalidPassword => Web::bad_request("Invalid password", "The password provided does not match with the user's password in the database"),
+            Error::Unauthorized => Web::forbidden("Unauthorized", "You are not logged in"),
+
         }
     }
 }
