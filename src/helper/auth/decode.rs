6648b87bb191c9bab7691dd5f586b15aa7d12616
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-14 17:55:10 +0700

    Added JWT encode and decode functions

---
+++ src/helper/auth/decode.rs
@@ -0,0 +1,30 @@
+use dotenv::var;
+use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
+use mongodb::bson::oid::ObjectId;
+
+use crate::Result;
+
+use super::{Claims, JwtType};
+
+pub fn decode_jwt(jwt: String, token_type: JwtType) -> Result<ObjectId> {
+    let jwt_secret = match token_type {
+        JwtType::Access => var("JWT_ACCESS").unwrap(),
+        JwtType::Refresh => var("JWT_REFRESH").unwrap(),
+    };
+
+    let key = DecodingKey::from_secret(jwt_secret.as_bytes());
+
+    let validation = Validation::new(Algorithm::HS512);
+
+    let decoded = decode::<Claims>(&jwt, &key, &validation)?;
+
+    Ok(decoded.claims.sub)
+}
+
+pub fn decode_access_token(jwt: String) -> Result<ObjectId> {
+    decode_jwt(jwt, JwtType::Access)
+}
+
+pub fn decode_refresh_token(jwt: String) -> Result<ObjectId> {
+    decode_jwt(jwt, JwtType::Refresh)
+}
---
+++ src/helper/auth/encode.rs
@@ -0,0 +1,38 @@
+use chrono::{Duration, Utc};
+use dotenv::var;
+use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
+
+use crate::{model::user::User, Result};
+
+use super::{Claims, JwtType};
+
+pub fn encode_jwt(user: &User, token_type: JwtType) -> Result<String> {
+    let (jwt_secret, duration) = match token_type {
+        JwtType::Access => (var("JWT_ACCESS").unwrap(), Duration::hours(1)),
+        JwtType::Refresh => (var("JWT_REFRESH").unwrap(), Duration::hours(3)),
+    };
+
+    let expiration = Utc::now().checked_add_signed(duration).unwrap().timestamp() as usize;
+
+    let claims = Claims {
+        sub: user.id,
+        name: user.username.clone(),
+        exp: expiration,
+    };
+
+    let header = Header::new(Algorithm::HS512);
+
+    let key = EncodingKey::from_secret(jwt_secret.as_bytes());
+
+    let encode = encode(&header, &claims, &key)?;
+
+    Ok(encode)
+}
+
+pub fn make_access_token(user: &User) -> Result<String> {
+    encode_jwt(user, JwtType::Access)
+}
+
+pub fn make_refresh_token(user: &User) -> Result<String> {
+    encode_jwt(user, JwtType::Refresh)
+}
