e896540e7c5b2e99985c3fd2c42222f971db3f03
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-14 17:54:27 +0700

    Added user profile handler

---
+++ src/handler/user/profile.rs
@@ -0,0 +1,13 @@
+use axum::extract::State;
+
+use crate::{request::user::loggedin::LoggedInUser, web::Web, SharedState, WebResult};
+
+pub async fn profile_user_handler(
+    State(SharedState { user_service }): State<SharedState>,
+    LoggedInUser(cookie_user): LoggedInUser,
+) -> WebResult {
+    Ok(Web::ok(
+        "User profile get successfully",
+        cookie_user.into_response(),
+    ))
+}
