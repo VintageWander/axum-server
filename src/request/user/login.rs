cad79d151b8c0bee661aac4c627f3ec8bfe560e7
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-14 17:57:17 +0700

    Added login user request

---
+++ src/request/user/login.rs
@@ -0,0 +1,35 @@
+use axum::{async_trait, body::Body, extract::FromRequest, http::Request, Json};
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+use crate::{
+    error::Error,
+    validation::user::{check_password, check_username},
+    SharedState,
+};
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+#[serde(rename_all = "camelCase")]
+pub struct LoginUserRequest {
+    #[validate(custom = "check_username")]
+    pub username: String,
+
+    #[validate(custom = "check_password")]
+    pub password: String,
+}
+
+#[async_trait]
+impl FromRequest<SharedState, Body> for LoginUserRequest {
+    type Rejection = Error;
+
+    async fn from_request(
+        req: Request<Body>,
+        state: &SharedState,
+    ) -> Result<Self, Self::Rejection> {
+        let Json(user_req) = Json::<LoginUserRequest>::from_request(req, state).await?;
+
+        user_req.validate()?;
+
+        Ok(user_req)
+    }
+}
