62cf1abe1f7ed0964a39bedca8ba69756d0895fc
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-19 17:17:24 +0700

    Declare modules

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,2 +1,3 @@
 pub mod auth;
+pub mod folder;
 pub mod user;
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,2 +1,14 @@
+use axum::Router;
+
+use crate::SharedState;
+
+use self::{auth::auth_routes, user::user_routes};
+
 pub mod auth;
 pub mod user;
+
+pub fn app_routes() -> Router<SharedState> {
+    Router::new()
+        .merge(auth_routes())
+        .merge(user_routes())
+}
