3a2ecbd428370411ce2481c7b5a6233c322ecb28
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-19 17:17:35 +0700

    Implement update folder handler

---
+++ src/handler/folder/update.rs
@@ -0,0 +1,28 @@
+use axum::extract::State;
+
+use crate::{
+    extractors::param::ParamID,
+    request::{folder::update::UpdateFolderRequest, user::loggedin::LoggedInUser},
+    web::Web,
+    SharedState, WebResult,
+};
+
+pub async fn update_folder_handler(
+    State(SharedState { folder_service, .. }): State<SharedState>,
+    ParamID(folder_id): ParamID,
+    LoggedInUser(cookie_user): LoggedInUser,
+    folder_req: UpdateFolderRequest,
+) -> WebResult {
+    let old_folder = folder_service
+        .get_folder_by_id_owner(&folder_id, &cookie_user)
+        .await?;
+
+    let target_folder = folder_req.into_folder_with_owner(old_folder, &cookie_user)?;
+
+    let updated_folder = folder_service
+        .update_folder(target_folder)
+        .await?
+        .into_response();
+
+    Ok(Web::ok("Update a folder successfully", updated_folder))
+}
