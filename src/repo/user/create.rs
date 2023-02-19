883c767ba0181953d330c48134ce927101d8bd96
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-19 13:06:01 +0700

    Transfer all validation logic to service layer

--- src/repo/user/create.rs
+++ src/repo/user/create.rs
@@ -1,19 +1,9 @@
-use tokio::try_join;
-
-use crate::{error::Error, model::user::User, Result};
+use crate::{model::user::User, Result};
 
 use super::UserRepo;
 
 impl UserRepo {
     pub async fn create_user(&self, user: User) -> Result<User> {
-        let (is_email_exists, is_username_exists) = try_join!(
-            self.exists_user_by_email(&user.email),
-            self.exists_user_by_username(&user.username)
-        )?;
-
-        if is_email_exists || is_username_exists {
-            return Err(Error::ConflictUser);
-        }
         self.user_dao.create_one(user).await
     }
 }
