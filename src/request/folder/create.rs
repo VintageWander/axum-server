8d94c7fadfbcc384ee11a32bd3aa266ef09479ad
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-19 17:20:37 +0700

    Added folder request model

---
+++ src/request/folder/create.rs
@@ -0,0 +1,54 @@
+use axum::{async_trait, body::Body, extract::FromRequest, http::Request, Json};
+
+use chrono::Utc;
+use mongodb::bson::oid::ObjectId;
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+use crate::{
+    error::Error,
+    model::{folder::Folder, user::User},
+    validation::file::*,
+    Result, SharedState,
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize, Validate)]
+#[serde(rename_all = "camelCase")]
+pub struct CreateFolderRequest {
+    #[validate(custom = "check_folder_name")]
+    pub folder_name: String,
+
+    #[validate(custom = "check_dir")]
+    pub position: String,
+
+    #[validate(custom = "check_visibility")]
+    pub visibility: String,
+}
+
+#[async_trait]
+impl FromRequest<SharedState, Body> for CreateFolderRequest {
+    type Rejection = Error;
+    async fn from_request(
+        req: Request<Body>,
+        state: &SharedState,
+    ) -> std::result::Result<Self, Self::Rejection> {
+        let Json(folder_req) = Json::<CreateFolderRequest>::from_request(req, state).await?;
+
+        folder_req.validate()?;
+
+        Ok(folder_req)
+    }
+}
+
+impl CreateFolderRequest {
+    pub fn into_folder_with_owner(self, owner: &User) -> Result<Folder> {
+        Folder::new(
+            &ObjectId::new(),
+            owner,
+            &self.folder_name,
+            &self.position,
+            self.visibility.try_into()?,
+            Utc::now().timestamp_millis(),
+        )
+    }
+}
---
+++ src/request/folder/update.rs
@@ -0,0 +1,52 @@
+use axum::{async_trait, body::Body, extract::FromRequest, http::Request, Json};
+
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+use crate::{
+    error::Error,
+    model::{folder::Folder, user::User},
+    validation::file::*,
+    Result, SharedState,
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize, Validate)]
+#[serde(rename_all = "camelCase")]
+pub struct UpdateFolderRequest {
+    #[validate(custom = "check_folder_name")]
+    pub folder_name: String,
+
+    #[validate(custom = "check_dir")]
+    pub position: String,
+
+    #[validate(custom = "check_visibility")]
+    pub visibility: String,
+}
+
+#[async_trait]
+impl FromRequest<SharedState, Body> for UpdateFolderRequest {
+    type Rejection = Error;
+    async fn from_request(
+        req: Request<Body>,
+        state: &SharedState,
+    ) -> std::result::Result<Self, Self::Rejection> {
+        let Json(folder_req) = Json::<UpdateFolderRequest>::from_request(req, state).await?;
+
+        folder_req.validate()?;
+
+        Ok(folder_req)
+    }
+}
+
+impl UpdateFolderRequest {
+    pub fn into_folder_with_owner(self, old_folder: Folder, owner: &User) -> Result<Folder> {
+        Folder::new(
+            &old_folder.id,
+            owner,
+            &self.folder_name,
+            &self.position,
+            self.visibility.try_into()?,
+            old_folder.created_at,
+        )
+    }
+}
