56755b103ad07c2784001095a3cbabf9f5c089b9
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-20 17:31:06 +0700

    Added create root folder in folder repo

--- src/repo/folder/create.rs
+++ src/repo/folder/create.rs
@@ -1,9 +1,23 @@
-use crate::{model::folder::Folder, Result};
+use tokio::try_join;
+
+use crate::{
+    model::{folder::Folder, user::User},
+    Result,
+};
 
 use super::FolderRepo;
 
 impl FolderRepo {
     pub async fn create_folder(&self, folder: Folder) -> Result<Folder> {
         self.folder_dao.create_one(folder).await
+    }
+
+    // This function is called when a new user is created
+    // So that everything has a parent folder
+    pub async fn create_root_folder(&self, owner: &User) -> Result<()> {
+        try_join!(self
+            .folder_dao
+            .create_one(Folder::new_root(owner)?))?;
+        Ok(())
     }
 }
