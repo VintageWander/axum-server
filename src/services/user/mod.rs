cf8d8454c0b6fcf349db0500b3f23fe0943c8925
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-20 17:36:05 +0700

    Inject folder service into user service

--- src/services/user/mod.rs
+++ src/services/user/mod.rs
@@ -6,15 +6,19 @@ pub mod update;
 
 use crate::{db::mongo::Mongo, repo::user::UserRepo};
 
+use super::folder::FolderService;
+
 #[derive(Debug, Clone)]
 pub struct UserService {
     user_repo: UserRepo,
+    folder_service: FolderService,
 }
 
 impl UserService {
     pub fn init(db: &Mongo) -> Self {
         Self {
             user_repo: UserRepo::init(db),
+            folder_service: FolderService::init(db),
         }
     }
 }
