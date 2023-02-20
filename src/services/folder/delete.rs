202e77d5b096db86c25d53b0cb48bb76f7b06ff3
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-20 17:32:35 +0700

    expose delete root folder in service layer

--- src/services/folder/delete.rs
+++ src/services/folder/delete.rs
@@ -1,9 +1,22 @@
-use crate::{model::folder::Folder, Result};
+use crate::{
+    model::{folder::Folder, user::User},
+    Result,
+};
 
 use super::FolderService;
 
 impl FolderService {
     pub async fn delete_folder(&self, folder: Folder) -> Result<()> {
-        self.folder_repo.delete_folder(folder).await
+        let deleted_folder = self.folder_repo.delete_folder(folder).await?;
+
+        self.folder_repo
+            .delete_folders_by_prefix_fullpath(&deleted_folder.fullpath)
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn delete_root_folder(&self, owner: &User) -> Result<()> {
+        self.folder_repo.delete_root_folder(owner).await
     }
 }
