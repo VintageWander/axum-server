943aaa0deded4ecdcc24bf62086dff44e0839385
Andrew Giang <nguyendtgiang@gmail.com>
2023-02-23 17:02:02 +0700

    Fix Mongo struct
    The Mongo struct now returns concrete value, since anything inside it fails, the app will crash

--- src/db/mongo.rs
+++ src/db/mongo.rs
@@ -1,4 +1,3 @@
-use crate::Result;
 use dotenv::var;
 use mongodb::{options::ClientOptions, Client, Collection};
 
@@ -7,13 +6,15 @@ pub struct Mongo {
 }
 
 impl Mongo {
-    pub async fn init() -> Result<Self> {
+    pub async fn init() -> Self {
         let mongo_uri = var("MONGODB_URI").expect("Cannot read mongodb uri from env");
-        let mut client_options = ClientOptions::parse(mongo_uri).await?;
+        let mut client_options = ClientOptions::parse(mongo_uri)
+            .await
+            .expect("Cannot parse MONGODB_URI");
         client_options.app_name = Some("simple-file-sharing".into());
-        Ok(Self {
-            client: Client::with_options(client_options)?,
-        })
+        Self {
+            client: Client::with_options(client_options).expect("Cannot create the MongoDB client"),
+        }
     }
 
     pub fn get_collection<T>(&self, collection_name: &str) -> Collection<T> {
