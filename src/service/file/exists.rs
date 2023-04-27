ddf3f69f231a39838c55055b965824a903fa5417
Andrew Giang <nguyendtgiang@gmail.com>
2023-04-27 16:04:49 +0700

    Added exists file by id function

--- src/service/file/exists.rs
+++ src/service/file/exists.rs
@@ -1,6 +1,12 @@
+use mongodb::bson::oid::ObjectId;
+
 use crate::{model::file::File, service::Service, validation::file::check_fullpath, Result};
 
 impl Service {
+    pub async fn exists_file_by_id(&self, file_id: ObjectId) -> Result<bool> {
+        self.file.exists_one(File::id(file_id)).await
+    }
+
     pub async fn exists_file_by_fullpath(&self, fullpath: &str) -> Result<bool> {
         check_fullpath(fullpath)?;
         self.file
