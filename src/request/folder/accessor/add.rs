72c4f57f9e600ef3991acb57afea6148d99c8deb
Andrew Giang <nguyendtgiang@gmail.com>
2023-04-27 16:03:29 +0700

    Implement add and remove accessor requests

---
+++ src/request/folder/accessor/add.rs
@@ -0,0 +1,28 @@
+use axum::{async_trait, body::Body, extract::FromRequest, http::Request, Json};
+
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+use crate::{error::Error, service::Service};
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+pub struct FolderAddAccessorRequest {
+    #[validate(email(message = "Invalid email"))]
+    pub email: String,
+}
+
+#[async_trait]
+impl FromRequest<Service, Body> for FolderAddAccessorRequest {
+    type Rejection = Error;
+    async fn from_request(req: Request<Body>, state: &Service) -> Result<Self, Self::Rejection> {
+        let Json(body) = Json::<FolderAddAccessorRequest>::from_request(req, state).await?;
+
+        body.validate()?;
+
+        if !state.exists_user_by_email(&body.email).await? {
+            return Err(Error::NotFound);
+        }
+
+        Ok(body)
+    }
+}
