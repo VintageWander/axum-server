769115661481dd80440a93af3259fe2261e52c8a
Andrew Giang <nguyendtgiang@gmail.com>
2023-04-27 16:31:47 +0700

    Mount extra handles

--- src/routes/file.rs
+++ src/routes/file.rs
@@ -5,8 +5,16 @@ use axum::{
 
 use crate::{
     handler::file::{
-        content::file_content_handler, create::create_file_handler, delete::delete_file_handler,
-        get::get_files_handler, restore::restore_file_handler, update::update_file_handler,
+        collaborator::{
+            add::add_file_collaborator_handler, get::get_file_collaborators_handler,
+            remove::remove_file_collaborator_handler,
+        },
+        content::file_content_handler,
+        create::create_file_handler,
+        delete::delete_file_handler,
+        get::{get_file_handler, get_files_handler},
+        restore::restore_file_handler,
+        update::update_file_handler,
         version::get_file_versions_handler,
     },
     service::Service,
@@ -17,17 +25,25 @@ pub fn file_routes() -> Router<Service> {
         "/files",
         Router::new()
             .merge(get_files_route())
+            .merge(get_file_route())
             .merge(get_file_versions_route())
             .merge(file_content_route())
             .merge(create_file_route())
             .merge(update_file_route())
             .merge(restore_file_route())
-            .merge(delete_file_route()),
+            .merge(delete_file_route())
+            .merge(get_file_collaborators_route())
+            .merge(add_file_collaborator_route())
+            .merge(remove_file_collaborator_route()),
     )
 }
 
 pub fn get_files_route() -> Router<Service> {
     Router::new().route("/", get(get_files_handler))
+}
+
+pub fn get_file_route() -> Router<Service> {
+    Router::new().route("/:file_id", get(get_file_handler))
 }
 
 pub fn file_content_route() -> Router<Service> {
@@ -52,4 +68,25 @@ pub fn restore_file_route() -> Router<Service> {
 
 pub fn delete_file_route() -> Router<Service> {
     Router::new().route("/:file_id", delete(delete_file_handler))
+}
+
+pub fn get_file_collaborators_route() -> Router<Service> {
+    Router::new().route(
+        "/:file_id/collaborator",
+        get(get_file_collaborators_handler),
+    )
+}
+
+pub fn add_file_collaborator_route() -> Router<Service> {
+    Router::new().route(
+        "/:file_id/collaborator",
+        post(add_file_collaborator_handler),
+    )
+}
+
+pub fn remove_file_collaborator_route() -> Router<Service> {
+    Router::new().route(
+        "/:file_id/collaborator",
+        delete(remove_file_collaborator_handler),
+    )
 }
--- src/routes/folder.rs
+++ src/routes/folder.rs
@@ -5,7 +5,13 @@ use axum::{
 
 use crate::{
     handler::folder::{
-        create::create_folder_handler, delete::delete_folder_handler, get::get_folders_handler,
+        collaborator::{
+            add::add_folder_collaborator_handler, get::get_folder_collaborator_handler,
+            remove::remove_folder_collaborator_handler,
+        },
+        create::create_folder_handler,
+        delete::delete_folder_handler,
+        get::{get_folder_handler, get_folders_handler},
         update::update_folder_handler,
     },
     service::Service,
@@ -16,14 +22,22 @@ pub fn folders_route() -> Router<Service> {
         "/folders",
         Router::new()
             .merge(get_folders_route())
+            .merge(get_folder_route())
             .merge(create_folder_route())
             .merge(update_folder_route())
-            .merge(delete_folder_route()),
+            .merge(delete_folder_route())
+            .merge(get_folder_collaborator_route())
+            .merge(add_folder_collaborator_route())
+            .merge(remove_folder_collaborator_route()),
     )
 }
 
 pub fn get_folders_route() -> Router<Service> {
     Router::new().route("/", get(get_folders_handler))
+}
+
+pub fn get_folder_route() -> Router<Service> {
+    Router::new().route("/:folder_id", get(get_folder_handler))
 }
 
 pub fn create_folder_route() -> Router<Service> {
@@ -36,4 +50,25 @@ pub fn update_folder_route() -> Router<Service> {
 
 pub fn delete_folder_route() -> Router<Service> {
     Router::new().route("/:folder_id", delete(delete_folder_handler))
+}
+
+pub fn get_folder_collaborator_route() -> Router<Service> {
+    Router::new().route(
+        "/:folder_id/collaborator",
+        get(get_folder_collaborator_handler),
+    )
+}
+
+pub fn add_folder_collaborator_route() -> Router<Service> {
+    Router::new().route(
+        "/:folder_id/collaborator",
+        post(add_folder_collaborator_handler),
+    )
+}
+
+pub fn remove_folder_collaborator_route() -> Router<Service> {
+    Router::new().route(
+        "/:folder_id/collaborator",
+        delete(remove_folder_collaborator_handler),
+    )
 }
