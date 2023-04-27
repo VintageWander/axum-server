334f8a5b3f270216248db7878b13dedaa89f4e6f
Andrew Giang <nguyendtgiang@gmail.com>
2023-04-27 14:28:10 +0700

    Now user route populates more data

--- src/handler/user/get.rs
+++ src/handler/user/get.rs
@@ -1,6 +1,12 @@
 use axum::extract::State;
 
-use crate::{model::user::*, service::Service, web::Web, WebResult};
+use crate::{
+    extractors::param::ParamID,
+    model::{populated::user::UserPopulated, user::*},
+    service::Service,
+    web::Web,
+    WebResult,
+};
 
 pub async fn get_users_handler(
     State(service): State<Service>,
@@ -14,4 +20,23 @@ pub async fn get_users_handler(
         .collect::<Vec<_>>();
 
     Ok(Web::ok("Get all users success", users))
+}
+
+pub async fn get_user_handler(
+    State(service): State<Service>,
+    ParamID(user_id): ParamID,
+) -> WebResult {
+    let user = service.get_user_by_id(user_id).await?;
+    let files = service.get_files_by_owner(&user).await?;
+    let shared_files = service
+        .get_shared_files_from_accessor(&user)
+        .await?;
+    let folders = service.get_folders_by_owner(&user).await?;
+    let shared_folders = service
+        .get_shared_folders_from_accessor(&user)
+        .await?;
+
+    let result = UserPopulated::new(user, files, shared_files, folders, shared_folders);
+
+    Ok(Web::ok("Get user success", result))
 }
