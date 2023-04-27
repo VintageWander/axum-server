379e0010a9af5c22d046a1473908a0382a61fdd7
Andrew Giang <nguyendtgiang@gmail.com>
2023-04-27 18:56:58 +0700

    Added bypass mechanism to update root folder

--- src/service/folder/update.rs
+++ src/service/folder/update.rs
@@ -6,7 +6,22 @@ impl Service {
     pub async fn update_folder(&self, folder: Folder) -> Result<Folder> {
         let old_folder = self.get_folder_by_id(folder.id).await?;
 
-        if old_folder.fullpath != folder.fullpath {
+        if folder.folder_name == folder.fullpath && folder.fullpath == folder.position {
+            // This complicated check is used to bypass normal security when updating
+            // the user's root folder
+            try_join!(
+                self.change_inner_folders_position(
+                    &old_folder.fullpath,
+                    &old_folder.fullpath,
+                    &folder.fullpath
+                ),
+                self.change_inner_files_position(
+                    &old_folder.fullpath,
+                    &old_folder.fullpath,
+                    &folder.fullpath
+                )
+            )?;
+        } else if old_folder.fullpath != folder.fullpath {
             let (is_folder_conflict, exists_parent_folder) = try_join!(
                 self.exists_folder_by_fullpath(&folder.fullpath), // Check for conflicts
                 self.exists_folder_by_fullpath(&folder.position), // Check for parent folder existence
