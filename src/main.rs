9c058888a2b825b114d0c5f9f34123e394dea1da
Andrew Giang <nguyendtgiang@gmail.com>
2023-04-30 22:38:26 +0700

    Added CORS to backend

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@
 
 use std::net::SocketAddr;
 
-use axum::{response::Response, Server};
+use axum::{
+    http::{
+        header::{
+            ACCEPT, ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS,
+            ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ORIGIN,
+        },
+        HeaderValue, Method,
+    },
+    response::Response,
+    Server,
+};
 
 use db::mongo::Mongo;
 use dotenv::{dotenv, var};
 use error::Error;
 
 use service::Service;
+use tower_http::cors::CorsLayer;
 
 use crate::routes::app_routes;
 
@@ -35,14 +46,39 @@ async fn main() -> Result<()> {
 
     let db = Mongo::init().await;
 
+    let origin = var("ORIGIN").expect("Cannot read the ORIGIN in the env");
+
+    let router = app_routes().with_state(Service::init(&db)).layer(
+        CorsLayer::new()
+            .allow_credentials(true)
+            .allow_origin(
+                origin
+                    .parse::<HeaderValue>()
+                    .expect("Failed to parse origin as HeaderValue"),
+            )
+            .allow_headers([
+                ORIGIN,
+                CONTENT_TYPE,
+                ACCEPT,
+                ACCESS_CONTROL_ALLOW_ORIGIN,
+                ACCESS_CONTROL_ALLOW_METHODS,
+                ACCESS_CONTROL_ALLOW_HEADERS,
+            ])
+            .allow_methods([
+                Method::GET,
+                Method::POST,
+                Method::PUT,
+                Method::DELETE,
+                Method::OPTIONS,
+            ]),
+    );
+
     let port = var("PORT")
         .expect("Cannot read the PORT in the env")
         .parse()
         .expect("Cannot convert PORT variable into u16");
 
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
-
-    let router = app_routes().with_state(Service::init(&db));
 
     Server::bind(&addr)
         .serve(router.into_make_service())
