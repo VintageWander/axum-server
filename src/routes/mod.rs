eccc48379110f803f4a0138455df9d0cd793b086
Andrew Giang <nguyendtgiang@gmail.com>
2023-04-30 22:38:44 +0700

    Fix CORS

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,10 @@
-use axum::Router;
+use axum::{
+    extract::{Path, State},
+    routing::options,
+    Router,
+};
 
-use crate::service::Service;
+use crate::{service::Service, web::Web, WebResult};
 
 use self::{auth::auth_routes, file::file_routes, folder::folders_route, user::user_routes};
 
@@ -9,8 +13,13 @@ pub mod file;
 pub mod folder;
 pub mod user;
 
+pub async fn ok_handler(State(_): State<Service>, Path(rest): Path<String>) -> WebResult {
+    Ok(Web::ok("OK", ()))
+}
+
 pub fn app_routes() -> Router<Service> {
     Router::new()
+        .route("/*rest", options(ok_handler))
         .merge(auth_routes())
         .merge(user_routes())
         .merge(folders_route())
