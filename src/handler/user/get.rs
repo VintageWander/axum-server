72734bfbf1c8658c16c8b8714b1cad93df807e50
Andrew Giang <nguyendtgiang@gmail.com>
2023-05-01 15:37:35 +0700

    The user populated model is not needed

--- src/handler/user/get.rs
+++ src/handler/user/get.rs
@@ -1,12 +1,8 @@
 use axum::extract::State;
 
 use crate::{
-    extractors::param::ParamID,
-    model::{populated::user::UserPopulated, user::*},
-    request::user::loggedin::LoggedInUser,
-    service::Service,
-    web::Web,
-    WebResult,
+    extractors::param::ParamID, model::user::*, request::user::loggedin::LoggedInUser,
+    service::Service, web::Web, WebResult,
 };
 
 pub async fn get_users_handler(
@@ -28,75 +24,7 @@ pub async fn get_user_handler(
     user_or_guest: Option<LoggedInUser>,
     ParamID(user_id): ParamID,
 ) -> WebResult {
-    // The user struct
-    let user = match &user_or_guest {
-        Some(LoggedInUser(user)) => user.clone(),
-        None => service.get_user_by_id(user_id).await?.clone(),
-    };
+    let user = service.get_user_by_id(user_id).await?;
 
-    // The user's public files
-    let public_files = service.get_public_files_by_owner(&user).await?;
-
-    // The user's shared files, owned by them
-    let my_shared_files = match &user_or_guest {
-        Some(LoggedInUser(user)) => service.get_shared_files_by_owner(user).await?,
-        None => vec![],
-    };
-
-    let other_shared_files = match &user_or_guest {
-        Some(LoggedInUser(user)) => {
-            service
-                .get_shared_files_from_collaborator(user)
-                .await?
-        }
-        None => vec![],
-    };
-
-    let private_files = match &user_or_guest {
-        Some(LoggedInUser(user)) => service.get_private_files_by_owner(user).await?,
-        None => vec![],
-    };
-
-    let public_folders = service.get_public_folders_by_owner(&user).await?;
-
-    let my_shared_folders = match &user_or_guest {
-        Some(LoggedInUser(user)) => service.get_shared_folders_by_owner(user).await?,
-        None => vec![],
-    };
-
-    let other_shared_folders = match &user_or_guest {
-        Some(LoggedInUser(user)) => {
-            service
-                .get_shared_folders_from_collaborator(user)
-                .await?
-        }
-        None => vec![],
-    };
-
-    let private_folders = match &user_or_guest {
-        Some(LoggedInUser(user)) => service
-            .get_private_folders_by_owner(user)
-            .await?
-            .into_iter()
-            .filter(|f| {
-                (f.folder_name != format!("{}/", user.username))
-                    && (f.fullpath != format!("{}/", user.username))
-            })
-            .collect(),
-        None => vec![],
-    };
-
-    let result = UserPopulated::new(
-        user,
-        public_files,
-        my_shared_files,
-        other_shared_files,
-        private_files,
-        public_folders,
-        my_shared_folders,
-        other_shared_folders,
-        private_folders,
-    );
-
-    Ok(Web::ok("Get user success", result))
+    Ok(Web::ok("Get user success", user.into_dto()))
 }
--- src/handler/user/profile.rs
+++ src/handler/user/profile.rs
@@ -1,55 +1,10 @@
 use axum::extract::State;
 
-use crate::{
-    model::populated::user::UserPopulated, request::user::loggedin::LoggedInUser, service::Service,
-    web::Web, WebResult,
-};
+use crate::{request::user::loggedin::LoggedInUser, service::Service, web::Web, WebResult};
 
 pub async fn profile_user_handler(
     State(service): State<Service>,
     LoggedInUser(user): LoggedInUser,
 ) -> WebResult {
-    // The user's public files
-    let public_files = service.get_public_files_by_owner(&user).await?;
-
-    // The user's shared files, owned by them
-    let my_shared_files = service.get_shared_files_by_owner(&user).await?;
-
-    let other_shared_files = service
-        .get_shared_files_from_collaborator(&user)
-        .await?;
-
-    let private_files = service.get_private_files_by_owner(&user).await?;
-
-    let public_folders = service.get_public_folders_by_owner(&user).await?;
-
-    let my_shared_folders = service.get_shared_folders_by_owner(&user).await?;
-
-    let other_shared_folders = service
-        .get_shared_folders_from_collaborator(&user)
-        .await?;
-
-    let private_folders = service
-        .get_private_folders_by_owner(&user)
-        .await?
-        .into_iter()
-        .filter(|f| {
-            (f.folder_name != format!("{}/", user.username))
-                && (f.fullpath != format!("{}/", user.username))
-        })
-        .collect();
-
-    let result = UserPopulated::new(
-        user,
-        public_files,
-        my_shared_files,
-        other_shared_files,
-        private_files,
-        public_folders,
-        my_shared_folders,
-        other_shared_folders,
-        private_folders,
-    );
-
-    Ok(Web::ok("Get user profile success", result))
+    Ok(Web::ok("Get user profile success", user.into_dto()))
 }
