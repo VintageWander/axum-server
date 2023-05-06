1330c49bfda22917134c0f926526af4e22738675
Andrew Giang <nguyendtgiang@gmail.com>
2023-05-06 10:04:17 +0700

    Fix error

--- src/error.rs
+++ src/error.rs
@@ -119,29 +119,29 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         match self {
             // Internal error
-            Error::Mongo(e) => Web::internal_error("MongoDB error", e).unwrap(),
-            Error::Jwt(e) => Web::internal_error("JWT error occur", format!("{}: means that you have to login", e)).unwrap(),
-            Error::Infallible(e) => Web::internal_error("Infallible error", "If you see this error please let me know").unwrap(),
-            Error::IO(e) => Web::internal_error("IO error", e).unwrap(),
+            Error::Mongo(e) => Web::internal_error("MongoDB error", e),
+            Error::Jwt(e) => Web::internal_error("JWT error occur", format!("{}: means that you have to login", e)),
+            Error::Infallible(e) => Web::internal_error("Infallible error", "If you see this error please let me know"),
+            Error::IO(e) => Web::internal_error("IO error", e),
 
             // AWS S3
-            Error::S3(e) => Web::internal_error("AWS S3 error", e).unwrap(),
+            Error::S3(e) => Web::internal_error("AWS S3 error", e),
             
             // Validation error
             Error::Fields(e) => Web::bad_request(
                 "Some of the fields are incorrect",
                 extract_validation_error(&e),
-            ).unwrap(),
-            Error::Field(e) => Web::bad_request("One of the field are incorrect", e).unwrap(),
-            Error::Multipart(e) => Web::bad_request("Invalid multipart form provided", e).unwrap(),
+            ),
+            Error::Field(e) => Web::bad_request("One of the field are incorrect", e),
+            Error::Multipart(e) => Web::bad_request("Invalid multipart form provided", e),
 
             
             // Bad requests error
-            Error::InvalidJson(e) => Web::bad_request("Invalid json form", e).unwrap(),
-            Error::InvalidQuery(e) => Web::bad_request("Invalid query form", e).unwrap(),
-            Error::InvalidId(e) => Web::bad_request("The id provided is invalid", e).unwrap(),
-            Error::InvalidPath(e) => Web::bad_request("Invalid path string provided", e).unwrap(),
-            Error::InvalidMultipart(e) => Web::bad_request("Invalid multipart form provided", e).unwrap(),
+            Error::InvalidJson(e) => Web::bad_request("Invalid json form", e),
+            Error::InvalidQuery(e) => Web::bad_request("Invalid query form", e),
+            Error::InvalidId(e) => Web::bad_request("The id provided is invalid", e),
+            Error::InvalidPath(e) => Web::bad_request("Invalid path string provided", e),
+            Error::InvalidMultipart(e) => Web::bad_request("Invalid multipart form provided", e),
 
 
             // Custom error
@@ -150,23 +150,23 @@ impl IntoResponse for Error {
             Error::NotFound => Web::not_found(
                 "Resource not found",
                 "Users, files, folders, or the version number provided could not be found",
-            ).unwrap(),
+            ),
 
             // User 
-            Error::ConflictUser => Web::conflict("User conflict", "This could mean that the username and email provided are already exists in the database").unwrap(),
-            Error::PasswordsMismatch => Web::bad_request("Passwords mismatch", "Password and confirm password fields are incorrect").unwrap(),
-            Error::InvalidPassword => Web::bad_request("Invalid password", "The password provided does not match with the user's password in the database").unwrap(),
-            Error::Unauthorized => Web::forbidden("Unauthorized", "You are not logged in").unwrap(),
-            Error::Split => Web::internal_error("Split error", "Cannot split the full filename to filename and extension").unwrap(),
+            Error::ConflictUser => Web::conflict("User conflict", "This could mean that the username and email provided are already exists in the database"),
+            Error::PasswordsMismatch => Web::bad_request("Passwords mismatch", "Password and confirm password fields are incorrect"),
+            Error::InvalidPassword => Web::bad_request("Invalid password", "The password provided does not match with the user's password in the database"),
+            Error::Unauthorized => Web::forbidden("Unauthorized", "You are not logged in"),
+            Error::Split => Web::internal_error("Split error", "Cannot split the full filename to filename and extension"),
             
             // Folder
-            Error::ConflictFolder => Web::conflict("Folder conflict", "This means that the folder with the same name in the exact position already exists, please try another name").unwrap(),
-            Error::ParentFolderNotFound => Web::not_found("Parent folder not found", "The folder that lives in the position provided in the new folder or file request could not be found").unwrap(),
-            Error::MoveToSelf => Web::not_found("Cannot move to self", "You cannot move a folder into itself, or moving parent folder into child").unwrap(),
+            Error::ConflictFolder => Web::conflict("Folder conflict", "This means that the folder with the same name in the exact position already exists, please try another name"),
+            Error::ParentFolderNotFound => Web::not_found("Parent folder not found", "The folder that lives in the position provided in the new folder or file request could not be found"),
+            Error::MoveToSelf => Web::not_found("Cannot move to self", "You cannot move a folder into itself, or moving parent folder into child"),
 
             // File
-            Error::ConflictFile => Web::conflict("File conflict", "This means that you may have a file with the same name living in the same folder").unwrap(),
-            Error::ExtensionDiff => Web::bad_request("Extension has been changed", "Changing the extension is not supported, as it might render the file unusable").unwrap(),
-        }
+            Error::ConflictFile => Web::conflict("File conflict", "This means that you may have a file with the same name living in the same folder"),
+            Error::ExtensionDiff => Web::bad_request("Extension has been changed", "Changing the extension is not supported, as it might render the file unusable"),
+        }.unwrap()
     }
 }
